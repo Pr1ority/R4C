114c74f9d46f4da4505cfd8595dc596d0f8c13f7
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-12-18 21:10:57 +0300

    update robots views

--- robots/views.py
+++ robots/views.py
@@ -16,7 +16,7 @@ def add_robot(request):
     """
     if request.method != 'POST':
         return JsonResponse(
-            {'ошибка': 'Некорректный метод запроса. Используйте метод POST.'},
+            {'ошибка': 'Некорректный метод запроса. Используйте POST.'},
             status=405
         )
 
