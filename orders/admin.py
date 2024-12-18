8d59952da9b3eca28480984dccbaaa51a1827ad3
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-12-18 19:13:31 +0300

    add requirements, robots urls, gitignore, update readme, robots views, models, admin, r4c urls, settings, orders admin, customers admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'robot_serial')
+    search_fields = ('customer__email', 'robot__serial')
